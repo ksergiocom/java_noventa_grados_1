a7c88f6249cd7827c1acb2e0a9d8bf7aff1cda42
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-11-07 19:37:53 +0100

    Update TableroConsultor.java
    Fuck You

--- src/noventagrados/control/TableroConsultor.java
+++ src/noventagrados/control/TableroConsultor.java
@@ -1,5 +1,9 @@
 package noventagrados.control;
 
+import java.util.Objects;
+
+import noventagrados.modelo.Celda;
+import noventagrados.modelo.Pieza;
 import noventagrados.modelo.Tablero;
 
 import noventagrados.util.Sentido;
@@ -18,6 +22,7 @@ public class TableroConsultor {
 		int diferenciaFilas = destino.fila() - origen.fila();
 		int diferenciaColumnas = destino.columna() - origen.columna();
 
+		// SOLO 1 RETURN SO BRUTO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		if (diferenciaFilas < 0 && diferenciaColumnas == 0) {
 			return Sentido.VERTICAL_N;
 		} else if (diferenciaFilas > 0 && diferenciaColumnas == 0) {
@@ -51,25 +56,178 @@ public class TableroConsultor {
 		}
 	}
 
+	/**
+	 * El método consultarNumeroPiezas retorna el número de piezas del tipo y color
+	 * indicado sobre el tablero.
+	 * 
+	 * @param tipoPieza
+	 * @param color
+	 * @return int (numero de piezas)
+	 */
 	public int calcularNumeroPiezas(TipoPieza tipoPieza, Color color) {
-		return 1;
+		int numPiezas = 0;
+		/*
+		 * El tablero tiene un método para recorrer todas las celdas Iteramos todas las
+		 * celdas y si son del tipo y color entonces sumamos el contador.
+		 */
+		// Esto lo hago por claridad
+		Celda[] arrayCeldasTablero = this.tablero.consultarCeldas();
+		// No se usar un foreach todavía
+		for(int i=0; i<arrayCeldasTablero.length; i++) {
+			Celda celdaIterada = arrayCeldasTablero[i];
+			// Eh! Me has metido un metodo consultarColorDePieza pero no un metodo ConsultarTipoPieza!!!!!!!!!
+			if(celdaIterada.consultarColorDePieza()==color && celdaIterada.consultarPieza().consultarTipoPieza() == tipoPieza) {
+				numPiezas++;
+			}
+		}
+
+		// SOY GILIPOLLAS, lo voy a dejar aquí para nunca olvidarlo
+		//		/* Voy a iterar todas las celdas posibles. Vamos a utilizar los metodos de consultar columnas y filas
+		//		 * para sacar el máximo de coordenadas por si cambiamos de tamñao de tablero.
+		//		 */
+		//		int numFilas = this.tablero.consultarNumeroFilas();
+		//		int numColumnas = this.tablero.consultarNumeroColumnas();
+		//		
+		//		for(int i=0; i<numFilas; i++) {
+		//			for(int j=0; j<numColumnas; j++) {
+		//				this.tablero.consult
+		//			}
+		//		}
+
+		return numPiezas;
 	}
 
+	/**
+	 * El método consultarNumeroPiezasEnHorizontal retorna el número de piezas contenidas en la 
+	 * misma horizontal de la coordenada dada, incluyendo a la pieza en dicha coordenada, si existe,
+	 * y con independencia del color de las piezas.
+	 * 
+	 * @param coordenada
+	 * @return int
+	 */
 	public int calcularNumeroPiezasEnHorizontal(Coordenada coordenada) {
-		return 1;
+		int numPiezas = 0;
+		// Primero comprobamos si la coordenada es válida
+		if(!this.tablero.estaEnTablero(coordenada)) return numPiezas;
+		
+		// En caso de que esté dentro del tablero. Busco el tamaño de la fila (será el n de columnas, porque
+		// presupongo que es cuadrada, todas las filas son del mismo tamaño.
+		int tamañoFila = this.tablero.consultarNumeroColumnas();
+		
+		// Itero las celdas de la fila y compruebo si tiene una pieza
+		for(int i=0; i<tamañoFila; i++) {
+			Coordenada coordeanadaAIterar = new Coordenada(coordenada.fila(), i);
+			Celda celdaIterada = this.tablero.consultarCelda(coordeanadaAIterar);
+			if(celdaIterada.consultarPieza() != null) numPiezas++;
+		}
+		
+		return numPiezas;
 	}
 
+	/**
+	 * El método consultarNumeroPiezasEnHorizontal retorna el número de piezas contenidas en la 
+	 * misma horizontal de la coordenada dada, incluyendo a la pieza en dicha coordenada, si existe,
+	 * y con independencia del color de las piezas.
+	 * 
+	 * @param coordenada
+	 * @return int
+	 */
 	public int calcularNumeroPiezasEnVertical(Coordenada coordenada) {
-		return 1;
+		int numPiezas = 0;
+		// Primero comprobamos si la coordenada es válida
+		if(!this.tablero.estaEnTablero(coordenada)) return numPiezas;
+		
+		// En caso de que esté dentro del tablero. Busco el tamaño de la columna (será el n de filas, porque
+		// presupongo que es cuadrada, todas las columnas son del mismo tamaño.
+		int tamanoColumna = this.tablero.consultarNumeroColumnas();
+		
+		// Itero las celdas de la fila y compruebo si tiene una pieza
+		for(int i=0; i<tamanoColumna; i++) {
+			Coordenada coordeanadaAIterar = new Coordenada(i, coordenada.columna());
+			Celda celdaIterada = this.tablero.consultarCelda(coordeanadaAIterar);
+			if(celdaIterada.consultarPieza() != null) numPiezas++;
+		}
+		
+		return numPiezas;
 	}
 
+	/**
+	 * Consulta si la reina del color indicado ocupa la celda central.
+	 * 
+	 * @param color
+	 * @return boolean
+	 */
 	public boolean estaReinaEnElCentro(Color color) {
+		// Vamos a hardcodear el centro a (3,3)
+		Coordenada coordenadaCentro = new Coordenada(3,3);
+		Celda celdaCentro = this.tablero.consultarCelda(coordenadaCentro);
+		
+		// NO QUIERO FORMATEAR ESTO! SE VE MEJOR ASí!!!!!!!!
+		if(
+				celdaCentro.consultarPieza() != null &&
+				celdaCentro.consultarPieza().consultarTipoPieza() == TipoPieza.REINA &&
+				celdaCentro.consultarColorDePieza() == color
+				) {
+			return true;
+		}
+		
+		// Si no coincide todo eso es que no está
 		return false;
 	}
 
+	/**
+	 * Comprueba si la reina del color indicado está todavía sobre el tablero.
+	 * 
+	 * @param color
+	 * @return boolean
+	 */
 	public boolean hayReina(Color color) {
+		// Buscamos todas las celdas
+		Celda[] celdasTablero = this.tablero.consultarCeldas();
+		
+		// Iteramos cada celda para comprobar su pieza
+		for(int i=0; i<celdasTablero.length; i++) {
+			// Por claridad lo vamos haciendo pasito a pasito
+			Celda celdaIterada = celdasTablero[i];
+			Pieza piezaDeLaCelda = celdaIterada.consultarPieza();
+			
+			// NO PIENSO FORMATEAR ESTO!!!!!!!!!! PORQUE SE VE MEJOR ASÍ!
+			// Si es la reina salirse con true
+			if(
+				piezaDeLaCelda != null &&
+				piezaDeLaCelda.consultarTipoPieza() == TipoPieza.REINA &&
+				piezaDeLaCelda.consultarColor() == color
+					) {
+				return true;
+			}
+			
+		}
+		
+		// Si no se ha encontrado es que false
 		return false;
 	}
 
-	// Falta el toString()
+	@Override
+	public int hashCode() {
+		return Objects.hash(tablero);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TableroConsultor other = (TableroConsultor) obj;
+		return Objects.equals(tablero, other.tablero);
+	}
+
+	@Override
+	public String toString() {
+		return "TableroConsultor [tablero=" + tablero + "]";
+	}
+
+	
 }
